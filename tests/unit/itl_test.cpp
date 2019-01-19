74041c15d4daee0aa505c19828b89deb2862721e
genshen <genshenchu@gmail.com>
2019-01-19 16:14:36 +0800

    test(inter): correct test for Itl::availTranDirs in tests/unit/itl_test.cpp

--- tests/unit/itl_test.cpp
+++ tests/unit/itl_test.cpp
@@ -32,24 +32,20 @@ TEST(itl_availTranDirs_test, itl_test) {
     data[7].type._type = LatticeTypes::Fe;
 
     // case 1
-    itl.availTranDirs(nei_status, _1nn);
-    EXPECT_EQ(itl.avail_trans_dir, 0xC3); // 0b 1100 0011
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn), 0xC3); // 0b 1100 0011
 
     // case 2
     data[0].type._type = LatticeTypes::FeFe; // not single atom.
-    itl.availTranDirs(nei_status, _1nn);
-    EXPECT_EQ(itl.avail_trans_dir, 0xC2); // 0b 1100 0010
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn), 0xC2); // 0b 1100 0010
 
     // case 3
     data[0].type._type = LatticeTypes::V; // not single atom.
-    itl.availTranDirs(nei_status, _1nn);
-    EXPECT_EQ(itl.avail_trans_dir, 0xC2); // 0b 1100 0010
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn), 0xC2); // 0b 1100 0010
 
     // case 4
     data[0].type._type = LatticeTypes::Fe; // recover
     data[3].type._type = LatticeTypes::FeCu;
-    itl.availTranDirs(nei_status, _1nn);
-    EXPECT_EQ(itl.avail_trans_dir, 0xC3); // no effect, 0b 1100 0010
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn), 0xC3); // no effect, 0b 1100 0010
     data[3].type._type = LatticeTypes::Fe;
 }
 
@@ -72,32 +68,27 @@ TEST(itl_availTranDirs_status_test, itl_test) {
     // case 1
     _type_neighbour_status nei_status = 0xFF;
     Lattice *_1nn_1[8] = {&data[0], &data[1], &data[2], &data[3], &data[4], &data[5], &data[6], &data[7]};
-    itl.availTranDirs(nei_status, _1nn_1);
-    EXPECT_EQ(itl.avail_trans_dir, 0x81);
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn_1), 0x81);
 
     // case 2
     nei_status = 0xF0; // 4 valid neighbour lattices.
     Lattice *_1nn_2[8] = {nullptr, nullptr, nullptr, nullptr, &data[4], &data[5], &data[6], &data[7]};
-    itl.availTranDirs(nei_status, _1nn_2);
-    EXPECT_EQ(itl.avail_trans_dir, 0x80); // 0b 1000 0000
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn_2), 0x80); // 0b 1000 0000
 
     // case 3
     nei_status = 0xF3; // 6 valid neighbour lattices.
     Lattice *_1nn_3[8] = {&data[0], &data[1], nullptr, nullptr, &data[4], &data[5], &data[6], &data[7]};
-    itl.availTranDirs(nei_status, _1nn_3);
-    EXPECT_EQ(itl.avail_trans_dir, 0x81); // 0b 1000 0001
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn_3), 0x81); // 0b 1000 0001
 
     // case 4
     nei_status = 0xFC; // 6 valid neighbour lattices.
     Lattice *_1nn_4[8] = {nullptr, nullptr, &data[2], &data[3], &data[4], &data[5], &data[6], &data[7]};
-    itl.availTranDirs(nei_status, _1nn_4);
-    EXPECT_EQ(itl.avail_trans_dir, 0x80);
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn_4), 0x80);
 
     // case 5
     nei_status = 0x0C; // 2 valid neighbour lattices.
     Lattice *_1nn_5[8] = {nullptr, nullptr, &data[2], &data[3], nullptr, nullptr, nullptr, nullptr,};
-    itl.availTranDirs(nei_status, _1nn_5);
-    EXPECT_EQ(itl.avail_trans_dir, 0x00);
+    EXPECT_EQ(itl.availTranDirs(nei_status, _1nn_5), 0x00);
 }
 
 TEST(itl_ratesIndex_test, itl_test) {
