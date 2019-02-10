4eec8c738a6971e498ff16f26622fe01a29a22a8
genshen <genshenchu@gmail.com>
2019-02-10 13:58:47 +0800

    feat(random): add xoshiro rng algorithm.

---
+++ src/utils/random/random.cpp
@@ -0,0 +1,5 @@
+//
+// Created by genshen on 2019-02-10.
+//
+
+#include "random.h"
---
+++ src/utils/random/random.h
@@ -0,0 +1,14 @@
+//
+// Created by genshen on 2019-02-10.
+//
+
+#ifndef MISA_KMC_RANDOM_H
+#define MISA_KMC_RANDOM_H
+
+
+class random {
+
+};
+
+
+#endif //MISA_KMC_RANDOM_H
