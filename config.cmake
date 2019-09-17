7db21a6dba4b05f05fcf045cbfb64519e93bbf3c
genshen <genshenchu@gmail.com>
2019-09-18 00:20:12 +0800

    build(cmake): enabled mpi to build parallel program pmisa-kmc.

--- config.cmake
+++ config.cmake
@@ -1,10 +1,9 @@
 set(KMC_VERSION "0.1.0")
 
 option(KMC_OpenMP_ENABLE_FLAG "Use OpenMP" OFF) #change this flag to OFF to disable OpenMP
-option(KMC_MPI_ENABLE_FLAG "Use MPI library" OFF) #change this flag to false to disable mpi
+option(KMC_MPI_ENABLE_FLAG "Use MPI library" ON) #change this flag to false to disable mpi
 option(KMC_TEST_BUILD_ENABLE_FLAG "Enable test" ON) # enable test
 option(KMC_TEST_MPI_ENABLE_FLAG "Enable MPI in test" ON) # enable mpi in test, its value depends on option MPI_ENABLE_FLAG.
-option(KMC_TOOLS_BUILD_ENABLE_FLAG "Enable tools building" ON) # building tools directory.
 
 set(KMC_RAND "MT" CACHE STRING "random number generating algorithm") # random number generating
 # options are:
