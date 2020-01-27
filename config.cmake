886a72d41eebe416bb640958c1b582dd225a7503
genshen <genshenchu@gmail.com>
2020-01-27 21:40:30 +0800

    feat(tools): add analysis tool to get count of isolated solute atoms from kmc dump file(xyz format)
    In our analysis tool, "isolated solute atom" means there is no solute atom in its 1nn range. Current
    tool can read a xyz file and print the count of isolated solute atom(Cu only now) in simulation box
    with periodic boundary condition.

--- config.cmake
+++ config.cmake
@@ -3,6 +3,7 @@ set(KMC_VERSION "0.1.0")
 option(KMC_OpenMP_ENABLE_FLAG "Use OpenMP" OFF) #change this flag to OFF to disable OpenMP
 option(KMC_MPI_ENABLE_FLAG "Use MPI library" ON) #change this flag to false to disable mpi
 option(KMC_TEST_BUILD_ENABLE_FLAG "Enable test" ON) # enable test
+option(KMC_TOOLS_BUILD_FLAG "Enable building tools" ON) # enable tools building
 option(KMC_TEST_MPI_ENABLE_FLAG "Enable MPI in test" ON) # enable mpi in test, its value depends on option MPI_ENABLE_FLAG.
 
 set(KMC_RAND "MT" CACHE STRING "random number generating algorithm") # random number generating
@@ -34,7 +35,10 @@ option(EAM_POT_ENABLE_FLAG "Enable using eam potential to calculate system energ
 set(KMC_MODEL_LIB_NAME abvi)
 set(PKMC_LIB_NAME pkmc)
 set(KMC_EXECUTE_BIN_NAME ${PROJECT_NAME})
+set(KMC_TOOLS_LIB_NAME kmc-tools)
+set(KMC_TOOLS_BIN_NAME tools)
 
 # test
 set(KMC_MODEL_UINT_TEST_NAME "abvi-model-unit-tests")
 set(PKMC_UINT_TEST_NAME "pkmc-unit-tests")
+set(TOOLS_UINT_TEST_NAME "tools-tests")
---
+++ tools/xyz_reader.h
@@ -0,0 +1,30 @@
+//
+// Created by genshen on 2020/1/26.
+//
+
+#ifndef MISA_KMC_XYZ_READER_H
+#define MISA_KMC_XYZ_READER_H
+
+
+#include <string>
+#include "../src/lattice/lattices_list.h"
+
+class XYZReader {
+public:
+    /**
+     * \brief read from file spcified by \param file_path and parsing it.
+     * save its parsing result too lattice list
+     * \param file_path
+     */
+    explicit XYZReader(std::string file_path);
+
+    inline LatticesList *getLatticeList() {
+        return lat_list;
+    }
+
+private:
+    LatticesList *lat_list = nullptr;
+};
+
+
+#endif //MISA_KMC_XYZ_READER_H
